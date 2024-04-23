17c786ca3984224b4ec7815160cc12c9edb689ea
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-04-23 16:54:45 +0700

    feat: homescreen done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -20,7 +20,10 @@ const Popularjobs = () => {
     num_pages: 1,
   });
 
-  console.log(data);
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {};
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -37,7 +40,13 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
             keyExtractor={(item) => item.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
